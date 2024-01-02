877e8ae62abee046ff77d2fca7d3b334927bef72
michal2396 <michal.milman@mail.huji.ac.il>
2024-01-02 21:33:43 +0200

    app done env file to be removed

--- constants.py
+++ constants.py
@@ -1,15 +1,20 @@
+from dotenv import load_dotenv
+import os
+
+# Load variables from .env file
+load_dotenv()
+
 # Keys
-languages_dic = {}
-scrape_API_KEY = 'a6c5e5fb-f434-433a-b585-9d0d7c4d1f33'
+scrape_API_KEY = os.getenv("SCRAPE_API_KEY")
 
 # Telegram API details
-telegram_api_key = '6460757943:AAHdW4SfJoLSz4Sx6kRqNPU8BSxm15J9S9g'
+telegram_api_key = os.getenv("TELEGRAM_API_KEY")
 telegram_user_name = 'chuck_norris_2396_bot'
-# Azure Translator API details
 
-subscription_key = '9d9565ce22244aada4e4130d7dbb2002'
-endpoint = 'https://api.cognitive.microsofttranslator.com/'
-region = 'southeastasia'
+# Azure Translator API details
+subscription_key = os.getenv("AZURE_SUBSCRIPTION_KEY")
+endpoint = os.getenv("AZURE_ENDPOINT")
+region = os.getenv("AZURE_REGION")
 
 # Nums
 SET_REQUEST = 1
@@ -20,3 +25,4 @@ UNDEFINED_MSG = 3
 SET_LANG_MSG = "You have to set language first!"
 DONT_UNDERSTAND_MSG = "I dont understand...."
 CHOOSE_LANG_MSG = "Hey! please choose a language"
+
